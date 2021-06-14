09d1200cb038283f1d507b7897d3324fb33afc5f
OpenHack <openhackuser@microsoft.com>
2021-06-14 18:23:30 +0200

    test

--- modules/azuredevops/example/modules.tf
+++ modules/azuredevops/example/modules.tf
@@ -2,8 +2,8 @@ module "azuredevops_projects" {
   source          = "../module"
   for_each        = var.azuredevops_projects
   project         = each.value
-  #global_settings = local.global_settings
-  #client_config   = local.client_config
+  global_settings = local.global_settings
+  client_config   = local.client_config
 }
 
 output azuredevops_projects {
