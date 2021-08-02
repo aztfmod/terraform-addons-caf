e88c6996e632b762546659937cf9cac17c67757a
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2021-08-02 17:08:51 +0200

    .

--- add-ons/glrunner/glrunner.tf
+++ add-ons/glrunner/glrunner.tf
@@ -22,5 +22,7 @@ module "glrunner" {
   #gitlab_server_cert        = lookup(each.value, "gitlab_server_cert", null)
   gitlab_server_internal_ip = lookup(each.value, "gitlab_server_internal_ip", null)
   gitlab_server_fqdn        = lookup(each.value, "gitlab_server_fqdn", null)
-  vnet_subnet               = lookup(each.value, "gitlab_server_subnet_name", null)  
+  vnet_subnet               = lookup(each.value, "gitlab_server_subnet_name", null)
+  
+  ci_workspace        = lookup(each.value, "ci_workspace", null)
 }
